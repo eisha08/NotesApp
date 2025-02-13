9c0d2cb4203008f748050098bfafae4bb8ebaf57
Eisha <eishajha0807@gmail.com>
2025-02-14 00:30:21 +0530

    Setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='text-2xl'>
+       Notes App 
+       </div>
+    </>
+  )
+}
+
+export default App
